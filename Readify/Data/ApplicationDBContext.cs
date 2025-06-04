aa18cccda24ae7b80e732dbaee9add74051cebab
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-06-04 13:36:05 +0300

    update category table with seeding data add bootstrap template

--- Readify/Data/ApplicationDBContext.cs
+++ Readify/Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Readify.Models;
 
 namespace Readify.Data
 {
@@ -6,6 +7,19 @@ namespace Readify.Data
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
+
+        }
+        public DbSet<Category> categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(new List<Category>
+            {
+                new Category { Id = 1,Name ="Fiction", DisplayOrder = 1 },
+                new Category { Id = 2,Name ="History", DisplayOrder = 2 },
+                new Category { Id = 3,Name ="Science", DisplayOrder = 3 },
+                new Category { Id = 4,Name ="Philosophy", DisplayOrder = 4},
+            });
 
         }
     }
--- Readify/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Readify.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
