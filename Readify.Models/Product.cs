aeb4e204bc7c4c4939419883aa9290522cafce4d
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-07-23 23:01:14 +0300

    Add Product View Model

--- Readify.Models/Product.cs
+++ Readify.Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Readify.Models
@@ -35,10 +36,11 @@ namespace Readify.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
-
+        [ValidateNever]
         public string ImageURL { get; set; } = "";
         public int CategoryID { get; set; }
         [ForeignKey("CategoryID")]
+        [ValidateNever]
         public Category Category { get; set; }
     }
 }
--- Readify/Areas/Admin/Controllers/ProductController.cs
+++ Readify/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Readify.DataAccess.Repository.Interfaces;
 using Readify.Models;
+using Readify.Models.ViewModels;
 namespace Readify.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
@@ -29,24 +30,45 @@ namespace Readify.Web.Areas.Admin.Controllers
             Text = c.Name,
             Value = c.Id.ToString()
         });
-            ViewData["categoryList"] = categoryList;
-            return View();
+            //ViewData["categoryList"] = categoryList;
+
+            ProductVM productVM = new()
+            {
+                Product = new Product(),
+                CategoryList = categoryList
+            };
+
+
+            return View(productVM);
         }
         [HttpPost]
-        public IActionResult Create(Product model)
+        public IActionResult Create(ProductVM model)
         {
 
             if (ModelState.IsValid)
             {
 
-                _unitOfWork.Product.Add(model);
+                _unitOfWork.Product.Add(model.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product Created successfully";
 
                 return RedirectToAction("Index", "Product");
             }
+            else
+            {
+                model.CategoryList = _unitOfWork.Category.GetAll()
+             .Select(c => new SelectListItem
+             {
+                 Text = c.Name,
+                 Value = c.Id.ToString()
+             });
 
-            return View();
+
+
+                return View(model);
+            }
+
+
         }
 
         public IActionResult Edit(int? id)
