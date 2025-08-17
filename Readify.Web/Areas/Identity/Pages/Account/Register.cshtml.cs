1e017c68d90ef2c80cd65553f790fac1fe7121d3
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-08-17 23:19:13 +0300

    Update register users by [Admin]

--- Readify.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Readify.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -203,7 +203,15 @@ namespace Readify.Web.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(StaticDetails.Role_Admin))
+                        {
+                            TempData["success"] = "New User Created Successfully";
+                        }
+                        else
+                        {
+
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
